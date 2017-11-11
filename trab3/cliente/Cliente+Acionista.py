bed9723082da926467f70bdd460ac858b611a1d9
Jimmy <jimmytsoares@gmail.com>
2017-11-11 20:01:35 -0200

    uptade cliente

--- trab3/cliente/Cliente+Acionista.py
+++ trab3/cliente/Cliente+Acionista.py
@@ -27,24 +27,24 @@ def rmv_str(test_str):
         elif skip1c == 0 and skip2c == 0 and skip3c == 0:
             ret += i
     return ret
-    
+
+print('Digite "comandos" para saber os comandos disponíveis ou "clear" para limpar a saída')
 while(1):
     entrada = input("Digite o comando: ")
-    checkentry = entrada.split(":")
-    if(checkentry[0] != 'consulta' or checkentry[0] != 'compra' or checkentry[0] != 'venda' or checkentry[0] != 'check'):
-        if(entrada=='comandos'):
-            print('Comandos disponíveis:')
-            print(' - Compra de ações   ->  compra:seuID:nomeAção:Preço')
-            print(' - Venda de ações    ->  venda:seuID:nomeAção:Preço')
-            print(' - Consulta de preço ->  consulta:seuID:nomeAção')
-            print(' - Checar status     ->  check')
-    else:
+    if(entrada !='comandos' and entrada != 'clear'):
         saida = 'http://localhost:8080/servidor/webresources/helloworld/' + entrada
         r = requests.get(saida)
-        print(saida)
+        #print(saida)
         print(rmv_str(r.text))
+    if(entrada=='comandos'):
+        print('Comandos disponíveis:')
+        print(' - Compra de ações   ->  compra:seuID:nomeAção:Preço')
+        print(' - Venda de ações    ->  venda:seuID:nomeAção:Preço')
+        print(' - Consulta de preço ->  consulta:seuID:nomeAção')
+        print(' - Checar status     ->  check:idTransação')
     if(entrada=='clear'):
         clear_output()
+        print('Digite "comandos" para saber os comandos disponíveis ou "clear" para limpar a saída')
 
 
 # In[ ]:
